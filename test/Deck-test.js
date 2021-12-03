060e6c25c84f10ef1cb863e542f0bbd23720b0ea
Jasmyn Hicks <jasmynh44@gmail.com>
2021-12-02 17:20:57 -0700

    Add and test method to count num of cards in deck

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -37,13 +37,13 @@ describe('Deck', function() {
     }]);
   });
 
-  it.skip('should know how many cards are in the deck', function() {
+  it('should know how many cards are in the deck', function() {
 
     const card1 = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const card2 = new Card(2, "What is a comma-separated list of related values?",  ["array", "object", "function"], "array");
 
     const deck = new Deck([card1, card2]);
 
-    expect(deck.countCards()).to.equal('2');
+    expect(deck.countCards()).to.equal(2);
   });
 })
